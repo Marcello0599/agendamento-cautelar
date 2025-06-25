be3c661b5237e4689c52c7abf75270bef3b18cab
Marcello0599 <marcello.couto@faculdadegran.edu.br>
2025-06-25 18:15:31 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// TODO: Substitua com as chaves do seu Firebase (te mostrarei como)
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  databaseURL: "https://SEU_DOMINIO.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_BUCKET.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 document.getElementById('loja').addEventListener('change', function () {
-  const outraLojaDiv = document.getElementById('outra-loja');
-  if (this.value === 'Outra') {
-    outraLojaDiv.style.display = 'block';
-  } else {
-    outraLojaDiv.style.display = 'none';
-  }
+  document.getElementById('outra-loja').style.display = this.value === 'Outra' ? 'block' : 'none';
 });
 
 document.getElementById('form-agendamento').addEventListener('submit', function (e) {
   e.preventDefault();
-  // Simula envio com Firebase no futuro
-  document.getElementById('mensagem-confirmacao').style.display = 'block';
-  this.reset();
-  document.getElementById('outra-loja').style.display = 'none';
+  const form = e.target;
+  const data = {
+    vendedor: form.vendedor.value,
+    loja: form.loja.value === 'Outra' ? form.outra_loja.value : form.loja.value,
+    placa: form.placa.value,
+    data: form.data.value,
+    horario: form.horario.value,
+    timestamp: new Date().toISOString()
+  };
+
+  const refPath = `agendamentos/${data.data}/${data.horario.replace(':', '-')}`;
+  firebase.database().ref(refPath).set(data).then(() => {
+    document.getElementById('mensagem-confirmacao').style.display = 'block';
+    form.reset();
+    document.getElementById('outra-loja').style.display = 'none';
+  }).catch((err) => {
+    alert('Erro ao agendar: ' + err.message);
+  });
 });
