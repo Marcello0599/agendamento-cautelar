1fbef3cb2d05fa22288e6a53ce4416d72400ebf9
Marcello0599 <marcello.couto@faculdadegran.edu.br>
2025-06-25 18:08:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.getElementById('loja').addEventListener('change', function() {
+document.getElementById('loja').addEventListener('change', function () {
   const outraLojaDiv = document.getElementById('outra-loja');
   if (this.value === 'Outra') {
     outraLojaDiv.style.display = 'block';
   } else {
     outraLojaDiv.style.display = 'none';
   }
+});
+
+document.getElementById('form-agendamento').addEventListener('submit', function (e) {
+  e.preventDefault();
+  // Simula envio com Firebase no futuro
+  document.getElementById('mensagem-confirmacao').style.display = 'block';
+  this.reset();
+  document.getElementById('outra-loja').style.display = 'none';
 });
